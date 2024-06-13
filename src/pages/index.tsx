1bf072aebea6e02c89a04e47f78bcde58ff8f20e
jiohjung98 <ckaquz98@naver.com>
2024-06-14 05:03:48 +0900

    design: main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Background from '@/components/Background';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 type HomeProps = {
   backgroundUrl: string;
@@ -11,13 +12,21 @@ type HomeProps = {
 const Home: React.FC<HomeProps> = ({ backgroundUrl }) => {
   const [startClicked, setStartClicked] = useState(false);
   const router = useRouter();
+  const [isContentVisible, setContentVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setContentVisible(true);
+    }, 1500); 
+  }, []);
 
   const handleStartClick = () => {
+    setStartClicked(true);
     router.push('/quiz');
   };
 
   return (
-    <div className="max-w-[500px] min-h-screen flex flex-col start-page text-white mx-auto start-font">
+    <div className="max-w-[500px] min-h-screen flex flex-col start-page text-white mx-auto start-font relative">
       <Head>
         <title>미스터리 수사단원 검증 미션</title>
         <meta name="description" content="Agent Of Mystery" />
@@ -29,27 +38,43 @@ const Home: React.FC<HomeProps> = ({ backgroundUrl }) => {
       </Head>
       <Background backgroundUrl={backgroundUrl} />
       <main className="flex-grow flex flex-col h-full items-center mx-auto px-4 py-4 mt-[140px] text-center z-[1]">
-        <h1 className="text-3xl mt-[10px]">
+        <motion.h1
+          className="text-3xl mt-[10px]"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: isContentVisible ? 1 : 0, y: isContentVisible ? 0 : -50 }}
+          transition={{ duration: 0.8 }} 
+        >
           무정부 국제 조직 XIN <br />( <span className="text-red-500">X </span><span className="text-red-500">I</span>nvestigation <span className="text-red-500">N</span>etwork )
-        </h1>
-        <p className="text-lg leading-relaxed mt-8 font-weight">
-          지구 상에서 일어나는 모든 미스터리한<br/>일들을 해결하는 비밀 조직 <span className="text-red-500">XIN</span>.<br />
-          인류가 지금까지 발견하지 못한<br/>X-전파를 통해 각종 사건을 수사한다.<br /><br/>
-          수사단 최정예 요원이 되기까지,<br/>마지막 한 단계만 남았다!<br />
-          현장에 투입된 나와 가장 닮은<br/>미스터리 수사단원은?
-        </p>
+        </motion.h1>
+        <motion.p
+          className="text-lg leading-relaxed mt-8 font-weight"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: isContentVisible ? 1 : 0, y: isContentVisible ? 0 : 50 }}
+          transition={{ duration: 0.8, delay: 0.4 }} 
+        >
+          지구 상에서 일어나는 모든 미스터리한<br />일들을 해결하는 비밀 조직 <span className="text-red-500">XIN</span>.<br />
+          인류가 지금까지 발견하지 못한<br />X-전파를 통해 각종 사건을 수사한다.<br /><br />
+          수사단 최정예 요원이 되기까지,<br />마지막 한 단계만 남았다!<br />
+          현장에 투입된 나와 가장 닮은<br />미스터리 수사단원은?
+        </motion.p>
       </main>
       {!startClicked && (
-        <div className='w-full px-6 z-[1]'>
+        <motion.div
+          className="absolute bottom-20 w-full px-6 z-[1]"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: isContentVisible ? 1 : 0, y: isContentVisible ? 0 : 50 }}
+          transition={{ duration: 0.8, delay: 0.6 }}
+        >
           <button
             onClick={handleStartClick}
-            className="red-button w-[90%]"
+            className="red-button w-[90%] mx-auto"
+            style={{ display: 'block' }}
           >
             검증 미션 시작하기
           </button>
-        </div>
+        </motion.div>
       )}
-        <footer className="absoulte bottom-0 text-center py-4 items-center z-10">
+      <footer className="absolute bottom-0 text-center py-4 items-center z-10 w-full">
         <Image src='/netflix-logo.png' alt='netflix' className='mx-auto' width={100} height={50} priority />
       </footer>
     </div>
