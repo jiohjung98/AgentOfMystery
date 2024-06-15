9dc9a402825421861137444f1d4dae7d877cf1a3
jiohjung98 <ckaquz98@naver.com>
2024-06-15 17:30:32 +0900

    feat: text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,7 +65,7 @@ const Home: React.FC<HomeProps> = ({ backgroundUrl }) => {
             animate={{ opacity: isContentVisible ? 1 : 0, y: isContentVisible ? 0 : -50 }}
             transition={{ duration: 0.8 }} 
           >
-            무정부 국제 조직 XIN <br />( <span className="text-red-500">X </span><span className="text-red-500">I</span>nvestigation <span className="text-red-500">N</span>etwork )
+            무정부 국제 조직 XIN <br />( <span className="text-red-700">X </span><span className="text-red-700">I</span>nvestigation <span className="text-red-700">N</span>etwork )
           </motion.h1>
           <motion.p
             className="font-hggothicssi-700 text-lg leading-relaxed mt-8 mb-4"
@@ -73,7 +73,7 @@ const Home: React.FC<HomeProps> = ({ backgroundUrl }) => {
             animate={{ opacity: isContentVisible ? 1 : 0, y: isContentVisible ? 0 : 50 }}
             transition={{ duration: 0.8, delay: 0.4 }} 
           >
-            지구 상에서 일어나는 모든 미스터리한<br />일들을 해결하는 비밀 조직 <span className="text-red-500">XIN</span>.<br />
+            지구 상에서 일어나는 모든 미스터리한<br />일들을 해결하는 비밀 조직 <span className="text-red-700">XIN</span>.<br />
             인류가 지금까지 발견하지 못한<br />X-전파를 통해 각종 사건을 수사한다.<br /><br />
             수사단 최정예 요원이 되기까지,<br />마지막 한 단계만 남았다!<br />
             현장에 투입된 나와 가장 닮은<br />미스터리 수사단원은?
